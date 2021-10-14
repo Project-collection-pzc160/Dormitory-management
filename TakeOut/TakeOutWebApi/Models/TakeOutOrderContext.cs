e4472743d2ace27ba11be9fb13c8899f9cc67ae9
pzc <2427438398@qq.com>
2021-10-14 18:44:19 +0800

    commit

---
+++ TakeOut/TakeOutWebApi/Models/TakeOutOrderContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DormManagementApi.Models
+{
+    public class TakeOutOrderContext : DbContext
+    {
+        public TakeOutOrderContext(DbContextOptions<TakeOutOrderContext> options)
+          : base(options)
+        {
+            this.Database.EnsureCreated(); //自动建库建表
+        }
+
+        public DbSet<TakeOutOrder> TakeOutOrders { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
